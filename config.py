b4f928d4096f7a21f0e175b2e24a82218c23f230
VELAPRCGOFOD000\CGAO <senavelez@gmail.com>
2024-02-19 09:23:19 -0500

    conexion

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # config.py
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@127.0.0.1:3306/Entrada1'
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:a5ba4g6C1bBG2Gec6dBBcHg6bCgbDF44@monorail.proxy.rlwy.net:16262/entrada'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@127.0.0.1:3306/Entrada1'
     #SQLALCHEMY_DATABASE_URI = 'mysql+mysqlclient://root:2hefbFfDEc-HFd1fhc2DhBCCgh-HCB65@monorail.proxy.rlwy.net:20020/Entrada1'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
